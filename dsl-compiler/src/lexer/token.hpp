532fad3330dc72a445377e5a6317df7ad46e3e80
Anirudh523 <aniaar523@gmail.com>
2026-08-06 18:17:57 -0700

    Completed writing the token_identifier enumeration, token struct, and the skeleton for the Lexer class. Now need to implement the functions that i declared.

---
+++ dsl-compiler/src/lexer/token.hpp
@@ -0,0 +1,17 @@
+#include <string_view>
+
+enum class TokenType {
+    Packet, Enum,
+    UInt8, UInt16, UInt32, Int8, Int16, Int32, Float32, Crc16,
+    LBrace, RBrace, LBracket, LParen, RParen, Semicolon, Comma,
+    Colon, Equals, At,
+    Identifier, IntegerLiteral,
+    Eof, Invalid
+};
+
+struct Token {
+    TokenType type;
+    std::string_view lexeme;
+    int line;
+    int column;
+}
